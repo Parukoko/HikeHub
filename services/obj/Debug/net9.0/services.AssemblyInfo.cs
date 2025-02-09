2af043bbd1f396ae1027ffd7fbc40138f704746f
unknown <thanatchayamook@gmail.com>
2025-02-09 22:38:25 +0700

    Save progress before switching branches

--- services/obj/Debug/net9.0/services.AssemblyInfo.cs
+++ services/obj/Debug/net9.0/services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b6e426432112678b991b6bb7d48526abf6916b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2826c44d72f1154b4dc7164117bae8c09a445fff")]
 [assembly: System.Reflection.AssemblyProductAttribute("services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
